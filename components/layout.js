6508b5b0e64700ab969d622562ae2004a5266028
Maulana Hilmi Arkan <maulanahelmy7@gmail.com>
2023-05-03 16:15:48 +0700

    for staging

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Hilmi Arkan";
+const name = "Maulana Hilmi Arkan";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({ children, home }) {
